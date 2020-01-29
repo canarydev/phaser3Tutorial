0121d021af717cf92fcf8e5e8c54e86e06e6dfae
Joshua M. González Santana <canary_styl3@hotmail.com>
2020-01-29 10:42:14 +0000

    Eliminado el spacing

--- src/Scenes/Scene2.js
+++ src/Scenes/Scene2.js
@@ -15,19 +15,11 @@ class Bootloader extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 25
         });
-
-        /* Cómo cargar un sprite que tiene espacios entre sprites */
-        this.load.spritesheet('tomato_spacing', 'tomato_spacing.png', {
-            frameWidth: 16,
-            frameHeight: 25,
-            margin: 1,
-            spacing: 2
-        });
     }
-    
+
     create() {
         this.tomato = this.add.sprite(100, 100, 'tomato', 0);
-        this.tomato_spacing = this.add.sprite(100, 130, 'tomato_spacing', 0);
+        
     }
     update(time, delta) {
        
